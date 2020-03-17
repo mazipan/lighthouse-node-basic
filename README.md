c3e9975972a861330417a9ba88b242cd38147e25
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-03-17 13:19:57 +0700

    feat: init lh 🚀

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # lighthouse-node-basic
+
 Running Lighthouse via Node.js and save the report in your file system
+
+## Run
+
+```
+$ yarn lh
+```
+
+## Read More
+
+- Read [Using Lighthouse programmatically](https://github.com/GoogleChrome/lighthouse/blob/master/docs/readme.md#using-programmatically) for help getting started.
---
+++ index.js
@@ -0,0 +1,33 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const fs = require('fs');
+const path = require('path');
+
+const TEST_URL = 'https://www.tokopedia.com/';
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then(results => {
+			const date = new Date().toISOString();
+			const p = path.resolve(`./reports/report-${date}.json`);
+			console.log('Report created on', p);
+			fs.writeFile(p, results.report, () => {});
+      // use results.lhr for the JS-consumable output
+      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+      // use results.report for the HTML/JSON/CSV output as a string
+      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+      return chrome.kill().then(() => results.lhr)
+    });
+  });
+}
+
+const opts = {
+	headless: true,
+  chromeFlags: ['--show-paint-rects', '--headless']
+};
+
+// Usage:
+launchChromeAndRunLighthouse(TEST_URL, opts).then(results => {
+  // Use results!
+});
